6f8058ec3473cda4cb5b6d038aa4d984e9085413
edmccrea <ed-mccrea@hotmail.co.uk>
2023-09-05 21:19:14 +0200

    refactor: update data and transitions

--- player-data.ts
+++ player-data.ts
@@ -44,6 +44,28 @@ export const playerData = {
       image:
         "https://www.bt.com/content/dam/bt/portal/images/articles/sport/world-cup-2022/Martinez6.jpg/jcr:content/renditions/landscape-desktop.764.430.jpg",
     },
+    {
+      href: "/players/paulo-dybala",
+      firstName: "Paulo",
+      lastName: "Dybala",
+      position: "ST",
+      number: 21,
+      caps: 50,
+      team: "AS Roma",
+      image:
+        "https://static0.givemesportimages.com/wordpress/wp-content/uploads/2022/12/2022-12-18T180359Z_2130738295_UP1EICI1E6MXR_RTRMADP_3_SOCCER-WORLDCUP-ARG-FRA-REPORT.jpg",
+    },
+    {
+      href: "/players/angel-correa",
+      firstName: "Angel",
+      lastName: "Correa",
+      position: "ST",
+      number: 15,
+      caps: 50,
+      team: "Atletico Madrid",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/JNJ1Cd33xuDhbaKVcNqjcETj868=/0x0:5323x3542/1200x0/filters:focal(0x0:5323x3542):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/24300859/1450125491.jpg",
+    },
   ],
   midfielders: [
     {
@@ -68,6 +90,72 @@ export const playerData = {
       image:
         "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt0e9953a760420a18/63923f382d164f58acba94a9/GettyImages-1446630221.jpg?auto=webp&format=pjpg&width=3840&quality=60",
     },
+    {
+      href: "/players/alexis-mac-allister",
+      firstName: "Alexis",
+      lastName: "Mac Allister",
+      position: "CM",
+      number: 20,
+      caps: 15,
+      team: "Brighton & Hove Albion",
+      image:
+        "https://i2-prod.irishmirror.ie/incoming/article28736350.ece/ALTERNATES/s615/0_GettyImages-1449038025.jpg",
+    },
+    {
+      href: "/players/leandro-paredes",
+      firstName: "Leandro",
+      lastName: "Paredes",
+      position: "CM",
+      number: 5,
+      caps: 50,
+      team: "Juventus",
+      image:
+        "https://gumlet.assettype.com/freepressjournal/2022-12/0a4d5a3a-dd28-40bb-aa85-8330deb6fd6c/parades.jpg",
+    },
+    {
+      href: "/players/exequiel-palacios",
+      firstName: "Exequiel",
+      lastName: "Palacios",
+      position: "CM",
+      number: 14,
+      caps: 15,
+      team: "Bayer Leverkusen",
+      image:
+        "https://images.teamtalk.com/content/uploads/2023/08/30095531/exequiel-palacios-holds-off-marko-livaja-during-argentina-vs-croatia.jpg",
+    },
+    {
+      href: "/players/guido-rodriguez",
+      firstName: "Guido",
+      lastName: "Rodriguez",
+      position: "CM",
+      number: 18,
+      caps: 15,
+      team: "Real Betis",
+      image:
+        "https://cdn1.molineux.news/uploads/2/2023/07/GettyImages-1245123680-1024x683.jpg",
+    },
+    {
+      href: "/players/alejandro-gomez",
+      firstName: "Alejandro",
+      lastName: "Gomez",
+      position: "CAM",
+      number: 17,
+      caps: 50,
+      team: "Sevilla",
+      image:
+        "https://mundoalbiceleste.com/wp-content/uploads/2022/12/alejandro-papu-gomez-argentina-national-team-australia-world-cup.jpg",
+    },
+    {
+      href: "/players/thiago-almada",
+      firstName: "Thiago",
+      lastName: "Almada",
+      position: "CAM",
+      number: 16,
+      caps: 1,
+      team: "Atlanta United",
+      image:
+        "https://images.mlssoccer.com/image/private/t_editorial_landscape_8_desktop_mobile/mls-atl-prd/injawnpswtmjowgntt7l.jpg",
+    },
   ],
   defenders: [
     {
@@ -81,6 +169,94 @@ export const playerData = {
       image:
         "https://d3nfwcxd527z59.cloudfront.net/content/uploads/2022/12/19103710/kylian-mbappe-argentina-752x428.jpg",
     },
+    {
+      href: "/players/nicolas-otamendi",
+      firstName: "Nicolas",
+      lastName: "Otamendi",
+      position: "CB",
+      number: 19,
+      caps: 50,
+      team: "Benfica",
+      image:
+        "https://www.the-sun.com/wp-content/uploads/sites/6/2022/12/crop-20710395.jpg?strip=all&quality=100&w=1920&h=1440&crop=1",
+    },
+    {
+      href: "/players/marcos-acuna",
+      firstName: "Marcos",
+      lastName: "Acuna",
+      position: "LB",
+      number: 8,
+      caps: 50,
+      team: "Sevilla",
+      image:
+        "https://i2-prod.birminghammail.co.uk/sport/football/football-news/article25630790.ece/ALTERNATES/s615/0_JS284816800.jpg",
+    },
+    {
+      href: "/players/gonzalo-montiel",
+      firstName: "Gonzalo",
+      lastName: "Montiel",
+      position: "RB",
+      number: 4,
+      caps: 15,
+      team: "Sevilla",
+      image:
+        "https://sevillafc.es/sites/default/files/news/image/montielpenalti.jpg",
+    },
+    {
+      href: "/players/nahuel-molina",
+      firstName: "Nahuel",
+      lastName: "Molina",
+      position: "RB",
+      number: 26,
+      caps: 15,
+      team: "Atletico Madrid",
+      image:
+        "https://digitalhub.fifa.com/transform/8569b608-ad82-49ea-ae97-8bcc8ac760ea/Netherlands-v-Argentina-Quarter-Final-FIFA-World-Cup-Qatar-2022",
+    },
+    {
+      href: "/players/lisandro-martinez",
+      firstName: "Lisandro",
+      lastName: "Martinez",
+      position: "CB",
+      number: 25,
+      caps: 15,
+      team: "Manchester United",
+      image:
+        "https://weallfollowunited.com/wp-content/uploads/2022/11/argentina-v-mexico-group-c-fifa-world-cup-qatar-2022-scaled.jpg",
+    },
+    {
+      href: "/players/nicolas-tagliafico",
+      firstName: "Nicolas",
+      lastName: "Tagliafico",
+      position: "LB",
+      number: 3,
+      caps: 50,
+      team: "Ajax",
+      image:
+        "https://dynaimage.cdn.cnn.com/cnn/digital-images/org/cb89e0e7-b1be-477a-8536-d632d6ca192a.jpg",
+    },
+    {
+      href: "/players/german-pezzella",
+      firstName: "German",
+      lastName: "Pezzella",
+      position: "CB",
+      number: 6,
+      caps: 15,
+      team: "Fiorentina",
+      image:
+        "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltae1c662e7b49b2aa/6393b6275acaa15c640655b6/pezzella.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+    {
+      href: "/players/juan-foyth",
+      firstName: "Juan",
+      lastName: "Foyth",
+      position: "CB",
+      number: 2,
+      caps: 15,
+      team: "Villarreal",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/ocpdJlBHEPqRC5Xon9vmtxusLmE=/20x40:2333x1825/1200x800/filters:focal(1265x1064:1637x1436)/cdn.vox-cdn.com/uploads/chorus_image/image/71766821/1245716638.0.jpg",
+    },
   ],
   goalkeepers: [
     {
@@ -92,7 +268,29 @@ export const playerData = {
       caps: 15,
       team: "Aston Villa",
       image:
-        "https://images.news18.com/ibnlive/uploads/2022/12/emiliano-martinez-ap-1.jpg",
+        "https://i.dailymail.co.uk/1s/2022/12/18/18/65708013-11551657-image-a-1_1671387302430.jpg",
+    },
+    {
+      href: "/players/franco-armani",
+      firstName: "Franco",
+      lastName: "Armani",
+      position: "GK",
+      number: 1,
+      caps: 15,
+      team: "River Plate",
+      image:
+        "https://fotos.perfil.com/2023/02/02/trim/1140/641/franco-armani-1502482.jpg",
+    },
+    {
+      href: "/players/geronimo-rulli",
+      firstName: "Gerónimo",
+      lastName: "Rulli",
+      position: "GK",
+      number: 12,
+      caps: 15,
+      team: "Villarreal",
+      image:
+        "https://image-service.onefootball.com/transform?w=280&h=210&dpr=2&image=https%3A%2F%2Fwp-images.onefootball.com%2Fwp-content%2Fuploads%2Fsites%2F10%2F2023%2F01%2FArgentina-v-France-Final-FIFA-World-Cup-Qatar-2022-1673032177-1000x699.jpg",
     },
   ],
 };
